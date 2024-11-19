7e1adbe14e4799a10c13afac3ba1c6ff427019ce
shivamkumarnayak <shivamkumarnayak100@gmail.com>
2024-11-19 17:34:05 +0530

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import BottomNavbar from './components/BottomNavbar/BottomNavbar'; 
 import TopNavbar from './components/TopNavbar/TopNavbar'; 
-// import WordToPDF from './components/WordToPDF/WordToPDF';
+import WordToPDF from './components/WordToPDF/WordToPDF';
 import ExcelToPDF from './components/ExcelToPDF/ExcelToPDF';
 
 function App() {
